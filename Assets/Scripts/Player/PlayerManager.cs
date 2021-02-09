848cf607091391e66c7d97fd2713a44577ee157f
Linus Vallinder <divinator.dev@gmail.com>
2021-02-09 11:20:13 +0100

    Player Joystick Movement is Done

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(PlayerMovement))]
+    public class PlayerManager : MonoBehaviour
+    {
+        [Header("Movement Settings")]
+        public float PlayerSpeed = 10.0f;
+
+        [Header("Movement Types")]
+        public MovementTypes ActiveMovementType = MovementTypes.JOYSTICK;
+
+        private PlayerMovement playerMovement;
+
+        private void Start()
+        {
+            playerMovement = GetComponent<PlayerMovement>();
+        }
+
+        private void Update()
+        {
+            switch (ActiveMovementType)
+            {
+                case MovementTypes.JOYSTICK:
+                    playerMovement.JoystickMovement(PlayerSpeed);
+                    break;
+            }
+        }
+    }
+
+    public enum MovementTypes
+    {
+        JOYSTICK
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private Rigidbody rb;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        #region Joystick
+
+        private Vector2 joystickOrigin;
+        private Vector2 joystickPosition;
+
+        public void JoystickMovement(float Speed)
+        {
+            if(Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                Vector3 touchScreenPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.transform.position.z));
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        joystickOrigin = touchScreenPosition;
+                        break;
+                    case TouchPhase.Ended:
+                        joystickOrigin = Vector2.zero;
+                        joystickPosition = Vector2.zero;
+                        break;
+                }
+
+                joystickPosition = touchScreenPosition;
+            }
+
+            Vector2 offset = joystickPosition - joystickOrigin;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+            rb.AddForce((new Vector3(direction.x, 0, direction.y) * -1) * Speed, ForceMode.Force);
+        }
+
+        #endregion
+    }
+}
